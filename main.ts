9edc45eac72cee3fc3750bc26ac073adc40e76b2
Zhiqin-Wei <zhiqin.daly.wei@mths.ca>
2024-10-06 18:14:30 -0400

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,17 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created on: oct
  * This program ...
 */
 
-basic.showString('Hello, World!')
+// Happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let count = 0
+
+input.onButtonPressed(Button.A, function () {
+    count += 1
+    basic.showNumber(count)
+})
